f61d80de7bcc38998f06eaa25455e928a02560c2
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-09-25 14:37:17 +0200

    Aggiunta ultima serie di if (funzionante)

--- Max_Min_Uguale.php
+++ Max_Min_Uguale.php
@@ -3,31 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Massimo, Minimo e Uguale</title>
 </head>
 <body>
     <?php
-        $x = rand(1,100);
-        $y = rand(1,100);
+        $x = rand(1,50);
+        $y = rand(1,50);
+        $max = 0;
         if ($x > $y) {
-            echo "<h1>La variabile $x è maggiore di $y</h1>"
+            echo "<h1>La variabile $x è maggiore di $y</h1>";
             $max = $x;
-        } else if ($x < $y) {
-            echo "<h1>La variabile $x è minore di $y</h1>"
+        } elseif ($x < $y) {
+            echo "<h1>La variabile $x è minore di $y</h1>";
             $max = $y;
         } else {
-            echo "<h1>La variabile $x è uguale a $y</h1>"
+            echo "<h1>La variabile $x è uguale a $y</h1>";
         }
-
-        if ($max <) {
-            # code... e
-        } elseif (condition) {
-            # code...
-        } elseif (condition) {
-            # code...
-        } else {
-            # code...
-        }
+    ?>
+    <?php
+        if ($max != 0) {
+            if ($max < 10) {
+                echo "<p>$max è minore di 10</p>";
+            } elseif ($max >= 10 && $max <= 20) {
+                echo "<p>$max è compreso tra 10 e 20</p>";
+            } elseif ($max >= 21 && $max <= 30) {
+                echo "<p>$max è compreso tra 21 e 30</p>";
+            } else {
+                echo "<p>$max è maggiore di 30</p>";
+            }
+        }  
     ?>
 </body>
 </html>
